e67db7a1ee287ac85617a50e53ed490e437320e6
CKS <communicate2charles@gmail.com>
2023-06-11 18:04:20 +0700

    endpoints/multiplayer plan for endpoints

--- backend/app/multiplayer/routes.py
+++ backend/app/multiplayer/routes.py
@@ -2,23 +2,23 @@ from flask import Blueprint, jsonify, request
 
 from database import db
 
-multiplayer_bp = Blueprint('multiplayer', __name__, url_prefix='/game/multiplayer')
+multiplayer_bp = Blueprint("multiplayer", __name__, url_prefix="/game/multiplayer")
 
 
-@multiplayer_bp.route('/waiting', methods=['POST'])
+@multiplayer_bp.route("/waiting", methods=["POST"])
 def wait_for_opponent():
     # wait for new player
     # start new session in new player or after x time return message with option try again / play vs computer
     pass
 
 
-@multiplayer_bp.route('/join', methods=['POST'])
+@multiplayer_bp.route("/join", methods=["POST"])
 def join_multiplayer_game():
     # find active session and join player or return unavailable offer game against computer
     pass
 
 
-@multiplayer_bp.route('/play', methods=['POST'])
+@multiplayer_bp.route("/play", methods=["POST"])
 def play_multiplayer_game():
     # make a move in multiplayer mode
     pass
